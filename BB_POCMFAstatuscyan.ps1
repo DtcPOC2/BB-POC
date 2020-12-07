24583619f7b15cfed2c3f4d43aa4658815de94ef
DtcPOC2 <74372198+DtcPOC2@users.noreply.github.com>
2020-12-07 15:15:42 +0000

    uploaded for testing #3 & 4

---
+++ BB_POCMFAstatuscyan.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "Results Count:" -BackgroundColor Cyan -ForegroundColor White
+
+Show-EventLog
+
+Register-ScheduledTask -TaskName "Schedule MFA Status Report" -Trigger $Time -Action $Action -RunLevel Highest
+
+Get-View -ViewType VirtualMachine -Filter @{"Name" = "BB POC CYAN MFA STATUS TESTING"}
+
+#BB keywords: MFA Status Cyan
---
+++ BB_POCMFAstatusdarkblue.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "Results Count:" -BackgroundColor DarkBlue -ForegroundColor White
+
+Show-EventLog
+
+Register-ScheduledTask -TaskName "Schedule MFA Status Report" -Trigger $Time -Action $Action -RunLevel Highest
+
+Get-View -ViewType VirtualMachine -Filter @{"Name" = "BB POC DARKBLUE MFA STATUS DARK BLUE TESTING"}
+
+#BB keywords: MFA Status darkblue dark blue
